7c99b90a1556deafe7c2dba6b971d68bd1cfa29a
samaphp <samaphp@gmail.com>
2018-04-25 11:08:31 +0300

    Defining the constructor.

--- hello.sol
+++ hello.sol
@@ -1,5 +1,15 @@
 pragma solidity ^0.4.17;
 
 contract hello {
+  // State variable.
+  uint temp;
+  // Person who will deploy the smart contract he will be the owner of this smart contract.
+  address owner;
 
+  // Since defining constructors as functions with the same name as the contract is deprecated.
+  // We will use sayHello().
+  function sayHello() public {
+    // msg is a global variable provided by the Solidity.
+    owner = msg.sender;
+  }
 }
